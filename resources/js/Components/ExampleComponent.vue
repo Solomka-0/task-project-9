378809028852737ae4c3e13e8af95f15105bfe8c
Максим Красюк <m.krasyuk@rave.dev>
2025-01-13 10:26:50 +0300

    Initial commit

---
+++ resources/js/Components/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<!-- resources/js/components/ExampleComponent.vue -->
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ExampleComponent',
+    data() {
+        return {
+            message: 'Привет из Vue 3 с Options API!',
+        };
+    },
+};
+</script>
+
+<style scoped>
+/* Стили компонента */
+</style>
